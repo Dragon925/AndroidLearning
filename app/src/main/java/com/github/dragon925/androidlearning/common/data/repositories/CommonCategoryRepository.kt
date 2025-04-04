887fb572ac7adb5ae88ef5cc2c3c19ad33d61f31
Александр Романов <romanov.alex99@yandex.ru>
2025-04-05 02:41:36 +0300

    Поправил замечания в практическом задании по Блоку XII (Flow)

--- app/src/main/java/com/github/dragon925/androidlearning/common/data/repositories/CommonCategoryRepository.kt
+++ app/src/main/java/com/github/dragon925/androidlearning/common/data/repositories/CommonCategoryRepository.kt
@@ -1,50 +1,31 @@
 package com.github.dragon925.androidlearning.common.data.repositories
 
 import android.content.res.AssetManager
-import android.util.Log
 import com.github.dragon925.androidlearning.common.data.datasorces.remote.AppClient
 import com.github.dragon925.androidlearning.common.data.models.CategoryDto
 import com.github.dragon925.androidlearning.common.data.toDomain
 import com.github.dragon925.androidlearning.common.domain.Category
-import com.google.gson.Gson
-import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.withContext
 
 object CommonCategoryRepository {
 
     private const val CATEGORY_FILE = "categories.json"
 
-    private val gson = Gson()
+    private val helper = LoadHelper(
+        fileName = CATEGORY_FILE,
+        jsonClass = Array<CategoryDto>::class.java
+    )
 
     fun getCategories(
         assets: AssetManager
-    ): Flow<List<Category>> = flow {
-        emit(AppClient.apiService.getCategories())
+    ): Flow<List<Category>> = helper.loadData(
+        assets = assets,
+        loader = AppClient.apiService::getCategories
+    ) { categories ->
+        categories.map(CategoryDto::toDomain)
+            .sortedBy(Category::name)
     }
-        .catch { emit(getCategoriesFromFile(assets)) }
-        .map { categories ->
-            categories.map(CategoryDto::toDomain)
-                .sortedBy { it.name }
-        }
         .flowOn(Dispatchers.IO)
-
-    private suspend fun getCategoriesFromFile(
-        assets: AssetManager,
-        dispatcher: CoroutineDispatcher = Dispatchers.IO
-    ): List<CategoryDto> = withContext(dispatcher) {
-        return@withContext try {
-            assets.open(CATEGORY_FILE).bufferedReader().use { inputStream ->
-                gson.fromJson(inputStream, Array<CategoryDto>::class.java).toList()
-            }
-        } catch (e: Exception) {
-            Log.e("CommonCategoryRepository-getCategories", "get categories failed", e)
-            emptyList()
-        }
-    }
 }
--- app/src/main/java/com/github/dragon925/androidlearning/common/data/repositories/CommonEventRepository.kt
+++ app/src/main/java/com/github/dragon925/androidlearning/common/data/repositories/CommonEventRepository.kt
@@ -1,66 +1,50 @@
 package com.github.dragon925.androidlearning.common.data.repositories
 
 import android.content.res.AssetManager
-import android.util.Log
 import com.github.dragon925.androidlearning.common.data.datasorces.remote.AppClient
 import com.github.dragon925.androidlearning.common.data.models.EventDto
 import com.github.dragon925.androidlearning.common.data.toDomain
 import com.github.dragon925.androidlearning.common.domain.Event
-import com.google.gson.Gson
-import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.withContext
 
 object CommonEventRepository {
 
     private const val EVENT_FILE = "events.json"
 
-    private val gson = Gson()
+    private val helper = LoadHelper(
+        fileName = EVENT_FILE,
+        jsonClass = Array<EventDto>::class.java
+    )
 
     fun getEvents(
         assets: AssetManager
-    ): Flow<List<Event>> = flow {
-        emit(AppClient.apiService.getEvents())
+    ): Flow<List<Event>> = helper.loadData(
+        assets = assets,
+        loader = AppClient.apiService::getEvents
+    ) { events ->
+        events.map(EventDto::toDomain)
+            .sortedWith(
+                compareBy(Event::startDate)
+                    .thenBy(Event::endDate)
+                    .thenBy(Event::name)
+            )
     }
-        .catch { emit(getEventsFromAssets(assets)) }
-        .map { events ->
-            events.map(EventDto::toDomain)
-                .sortedWith(
-                    compareBy<Event> { it.startDate }
-                        .thenBy { it.endDate }
-                        .thenBy { it.name }
-                )
-        }
         .flowOn(Dispatchers.IO)
 
     fun getEventById(
         eventId: String,
         assets: AssetManager
-    ): Flow<Event> = flow {
-        emit(AppClient.apiService.getEvent(eventId))
-    }
-        .catch {
-            emit(getEventsFromAssets(assets).first { it.id == eventId })
+    ): Flow<Event> = helper.loadFirstData(
+        assets = assets,
+        loader = {
+            AppClient.apiService.getEvent(eventId)
+        },
+        mapper = EventDto::toDomain,
+        predicate = { event ->
+            event.id == eventId
         }
-        .map(EventDto::toDomain)
+    )
         .flowOn(Dispatchers.IO)
-
-    private suspend fun getEventsFromAssets(
-        assets: AssetManager,
-        dispatcher: CoroutineDispatcher = Dispatchers.IO
-    ): List<EventDto> = withContext(dispatcher) {
-        return@withContext try {
-            assets.open(EVENT_FILE).bufferedReader().use { inputStream ->
-                gson.fromJson(inputStream, Array<EventDto>::class.java).toList()
-            }
-        } catch (e: Exception) {
-            Log.e("CommonEventRepository-getEvents", "get events failed", e)
-            emptyList()
-        }
-    }
 }
